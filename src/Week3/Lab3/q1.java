c14a55361c00c49ccc576432d4e3bd3937aa4d58
rach <bbazzuraa@gmail.com>
2025-11-03 10:07:58 +0800

    re-code the q1 cuz its missing

--- src/Week3/Lab3/q1.java
+++ src/Week3/Lab3/q1.java
@@ -1,4 +1,32 @@
 package Week3.Lab3;
 
+import java.util.Scanner;
+
 public class q1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        input.nextLine();
+
+        System.out.print("Enter the operand: ");
+        char operand = input.nextLine().charAt(0);
+
+        if (operand == '+') {
+            System.out.print(num + num2);
+        } else if (operand == '-') {
+            System.out.print(num - num2);
+        } else if (operand == '*') {
+            System.out.print(num * num2);
+        } else if (operand == '/') {
+            System.out.print(num / num2);
+        } else  {
+            System.out.println("Invalid input");
+        }
+    }
+
 }
