a3820954c9126a4fb541369d034d39187fa6018a
rach <bbazzuraa@gmail.com>
2025-11-09 03:01:17 +0800

    q4 and q5

--- src/Week4/Lab4/q4.java
+++ src/Week4/Lab4/q4.java
@@ -1,22 +1,49 @@
 package Week4.Lab4;
 
 import java.util.Scanner;
-import java.time.LocalDate;
-import static java.time.Year.isLeap;
 
 public class q4 {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int year = 0, d1 = 0;
 
-        System.out.print("Enter year: ");
+        System.out.print("enter the year: ");
         year = input.nextInt();
 
-        System.out.print("Enter first day of the year: ");
+        System.out.print("enter the first day of the year: ");
         d1 = input.nextInt();
 
-        int daysToMay = 31 + (isLeap(year) ? 29 : 28) + 31 + 30;
-        int daysToAug = daysToMay + 31 + 30 + 31;
+        int daystoMay = 31 + (isLeap(year) ? 29 : 28) + 31 + 30;
+        int daystoAug = daystoMay + 31 + 30 + 31;
+
+        displayCalendar((d1 + daystoMay) % 7, year, "May", 31);
+        displayCalendar((d1 + daystoAug) % 7, year, "August", 31);
+    }
+
+    static boolean isLeap(int year) {
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+    }
+
+    static void displayCalendar(int dayinWeek, int year, String month, int daysinMonth) {
+
+        System.out.printf("\nCalendar for %s %d\n", month, year);
+        System.out.println("┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐");
+        System.out.println("│ Sun │ Mon │ Tue │ Wed │ Thu │ Fri │ Sat |");
+        System.out.println("├─────┼─────┼─────┼─────┼─────┼─────┼─────┤");
+        System.out.print(("│     ").repeat(dayinWeek));
+
+        for (int i = 1; i <= daysinMonth; i++) {
+
+            System.out.printf("│ %s  ", (i < 10) ? "0" + i : i);
+            dayinWeek++;
+
+            if (dayinWeek % 7 == 0) {
+                System.out.print("│\n├─────┼─────┼─────┼─────┼─────┼─────┼─────┤\n");
+                dayinWeek = 0;
+            }
+        }
+
+        System.out.print(("│     ").repeat(7 - dayinWeek) + "│\n└─────┴─────┴─────┴─────┴─────┴─────┴─────┘\n");
 
     }
 }
--- src/Week4/Lab4/q5.java
+++ src/Week4/Lab4/q5.java
@@ -1,4 +1,20 @@
 package Week4.Lab4;
 
+import java.util.Scanner;
+
 public class q5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int score1 = 0, score2 = 0, roll = 0;
+        boolean p1Turn = true;
+
+        while (Math.max(score1, score2) <= 100) {
+            roll = (int) (Math.random() * (6 - 1) + 1);
+            if (p1Turn) { score1 += roll; } else { score2 += roll; }
+            System.out.printf("Player 1 Score: %d, Player 2 Score: %d\n", score1, score2);
+            p1Turn = !p1Turn;
+        }
+
+        System.out.printf("Player %d won!", score1 > score2 ? 1 : 2);
+    }
 }
