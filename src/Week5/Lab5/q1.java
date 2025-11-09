49659fc216043a1db69d809d616568126476d278
rach <bbazzuraa@gmail.com>
2025-11-09 16:33:52 +0800

    adding package for tutorial and lab 5

---
+++ src/Week5/Lab5/q1.java
@@ -0,0 +1,7 @@
+package Week5.Lab5;
+
+public class q1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Week5/Tutorial5/q1.java
@@ -0,0 +1,5 @@
+package Week5.Tutorial5;
+
+public class q1 {
+    public static void main(String[] args) {}
+}
