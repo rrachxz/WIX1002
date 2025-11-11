dc67511d22dcdc7eb2b44cffe7f3b828a532c774
rach <bbazzuraa@gmail.com>
2025-11-11 18:41:29 +0800

    update

--- src/Week4/Lab4/q5.java
+++ src/Week4/Lab4/q5.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class q5 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int score1 = 0, score2 = 0, roll = 0;
-        boolean p1Turn = true;
-
-        while (Math.max(score1, score2) <= 100) {
-            roll = (int) (Math.random() * (6 - 1) + 1);
-            if (p1Turn) { score1 += roll; } else { score2 += roll; }
-            System.out.printf("Player 1 Score: %d, Player 2 Score: %d\n", score1, score2);
-            p1Turn = !p1Turn;
+        int p1 = 0, p2 = 0, dice; boolean isOneTurn = true;
+        while(Math.max(p1, p2) <= 100) {
+            dice = (int) (Math.random() * 6 + 1);
+            if(isOneTurn) {
+                p1 += dice;
+                isOneTurn = dice == 6 ? true : false;
+            } else {
+                p2 += dice;
+                isOneTurn = dice == 6 ? false : true;
+            }
         }
-
-        System.out.printf("Player %d won!", score1 > score2 ? 1 : 2);
+        System.out.printf("%s win with %d points!\n", (p1 > p2 ? "Player 1" : "Player 2"), Math.max(p1, p2));
     }
 }
