6a3b8fa6ad245ebc40b598a7c8a7c6dac5338aac
rach <bbazzuraa@gmail.com>
2025-11-18 12:47:07 +0800

    added week6 package

---
+++ src/Week6/Lab6/q1.java
@@ -0,0 +1,7 @@
+package Week6.Lab6;
+
+public class q1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Week6/Tutorial6/q1.java
@@ -0,0 +1,7 @@
+package Week6.Tutorial6;
+
+public class q1 {
+    public static void main(String[] args) {
+
+    }
+}
